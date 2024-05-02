3d953dfa419e93691d8160f3527d9c1e5633da97
Santiago_Firigua <santiagofiriguapalma@gmail.com>
2024-05-02 07:54:01 -0500

    login y video

--- js/login.js
+++ js/login.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const username = document.getElementById('username');
     const password = document.getElementById('password');
     const btn = document.getElementById('submitBtn');
+    const redirectLink = document.getElementById('redirectLink');
 
     // Evento clic en el botón
     btn.addEventListener('click', function(e) {
@@ -12,32 +13,15 @@ document.addEventListener("DOMContentLoaded", function() {
             dataspw: password.value
         };
         console.log(datos);
-        // Aquí puedes agregar la lógica para enviar el formulario a través de AJAX o cualquier otro método de envío
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    const loginForm = document.getElementById("loginForm");
-    const redirectLink = document.getElementById("redirectLink");
-
-    // Evento envío del formulario
-    loginForm.addEventListener("submit", function(event) {
-        event.preventDefault(); // Evita que el formulario se envíe automáticamente
-
-        // Aquí puedes agregar la lógica para enviar el formulario a través de AJAX o simplemente proceder con la redirección
-        // En este ejemplo, simplemente mostraremos el enlace y redireccionaremos después de un breve retraso
-        redirectLink.style.display = "block";
-
-        // Simulando un breve retraso antes de redireccionar (puedes quitar esto en tu código real)
+        // Simulación de inicio de sesión exitoso
         setTimeout(function() {
-            window.location.href = redirectLink.href;
-        }, 2000); // Redirige después de 0.7 segundos (700 milisegundos)
+            // Muestra la alerta de inicio de sesión
+            alert("Se inició sesión correctamente");
+            // Redirige a la página anterior después de 3 segundos
+            setTimeout(function() {
+                redirectLink.click();
+            }, 3000);
+        }, 1000); // Simula un inicio de sesión exitoso después de 1 segundo
     });
 });
-
-
-
-
-
-
 
